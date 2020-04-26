17dd3ee829009d8d2f8ae452afbee3185aa2970e
16lemoing <guillaume.le_moing@mines-paristech.fr>
2020-04-26 20:33:07 +0200

    update script

--- script/experiments.sh
+++ script/experiments.sh
@@ -21,30 +21,49 @@ cd ../bin
 #python3 run_simulation.py --name batch_size --lr 0.01 --normalize_scores --batch_size 128
 #python3 run_simulation.py --name batch_size --lr 0.01 --normalize_scores --batch_size 256
 
-# set batch_size to 128
+# set batch_size to 256
 
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type word2vec
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type glove
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type random
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type word2vec --correct_spelling
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type glove --correct_spelling
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type random --correct_spelling
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type word2vec --correct_spelling --dim 100
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type glove --correct_spelling --dim 100
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type random --correct_spelling --dim 100
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type word2vec --correct_spelling --dim 200
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type glove --correct_spelling --dim 200
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type random --correct_spelling --dim 200
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type word2vec --correct_spelling --dim 300
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type glove --correct_spelling --dim 300
-#python3 run_simulation.py --name embedding --lr 0.01 --normalize_scores --batch_size 128 --embedding_type random --correct_spelling --dim 300
+python3 run_simulation.py --name spelling --lr 0.01 --normalize_scores --batch_size 256
+python3 run_simulation.py --name spelling --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling
 
 # set correct_spelling to true
+
+#python3 run_simulation.py --name feat --lr 0.01 --normalize_scores --batch_size 256
+#python3 run_simulation.py --name feat --lr 0.01 --normalize_scores --batch_size 256 --use_features
+#python3 run_simulation.py --name feat --lr 0.01 --normalize_scores --batch_size 256 --use_features --scale_features
+
+# set use_features to true
+# set scale_features to true
+
+#python3 run_simulation.py --name feat --lr 0.01 --normalize_scores --batch_size 256 --use_features --scale_features --dim 50
+#python3 run_simulation.py --name feat --lr 0.01 --normalize_scores --batch_size 256 --use_features --scale_features --dim 300
+
 # set dim to 300
 
-python3 run_simulation.py --name features --lr 0.01 --normalize_scores --batch_size 128 --correct_spelling --dim 300
-python3 run_simulation.py --name features --lr 0.01 --normalize_scores --batch_size 128 --correct_spelling --dim 300 --use_features
-python3 run_simulation.py --name features --lr 0.01 --normalize_scores --batch_size 128 --correct_spelling --dim 300 --use_features --scale_features
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 10 16
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 100 16
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 300 16
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 300 128
+
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 10 16 --dropout 0.2
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 100 16 --dropout 0.2
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 300 16 --dropout 0.2
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 300 128 --dropout 0.2
+
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 10 16 --dropout 0.4
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 100 16 --dropout 0.4
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 300 16 --dropout 0.4
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 300 128 --dropout 0.4
+
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 10 16 --dropout 0.6
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 100 16 --dropout 0.6
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 300 16 --dropout 0.6
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 300 128 --dropout 0.6
+
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 10 16 --dropout 0.8
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 100 16 --dropout 0.8
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 300 16 --dropout 0.8
+python3 run_simulation.py --name dense --lr 0.01 --normalize_scores --batch_size 256 --correct_spelling --dim 300 --use_features --scale_features --hidden_size 300 128 --dropout 0.8
 
 #--dim
 #--remove_stopwords
